58f4f7b5f84efa7570cfc0d042fe96e72ecce4e9
ARC-22 <advaitchuri22@gmail.com>
2025-02-09 18:14:14 -0800

    added circuit breaker and product service documentation

--- api-gateway/src/main/java/com/arc/microservices/gateway/route/Routes.java
+++ api-gateway/src/main/java/com/arc/microservices/gateway/route/Routes.java
@@ -1,12 +1,16 @@
 package com.arc.microservices.gateway.route;
 
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
 
 @Configuration
 public class Routes {
@@ -15,6 +19,7 @@ public class Routes {
     public RouterFunction<ServerResponse> productServiceRoute() {
         return GatewayRouterFunctions.route("product_service")
                 .route(RequestPredicates.path("/api/product"), HandlerFunctions.http("http://localhost:8080"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
@@ -22,6 +27,7 @@ public class Routes {
     public RouterFunction<ServerResponse> orderServiceRoute() {
         return GatewayRouterFunctions.route("porder_service")
                 .route(RequestPredicates.path("/api/order"), HandlerFunctions.http("http://localhost:8081"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("orderServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
@@ -29,6 +35,22 @@ public class Routes {
     public RouterFunction<ServerResponse> inventoryServiceRoute() {
         return GatewayRouterFunctions.route("inventory_service")
                 .route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http("http://localhost:8082"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("inventoryServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
+                .build();
+    }
+//
+//    @Bean
+//    public RouterFunction<ServerResponse> fallbackRoute() {
+//        return GatewayRouterFunctions.route("fallbackRoute")
+//                .GET("/fallbackRoute", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+//                        .body("Service unavailable, pleas try again later")
+//                .build();
+//    }
+    @Bean
+    public RouterFunction<ServerResponse> fallbackRoute() {
+        return GatewayRouterFunctions.route("fallbackRoute")
+                .GET("/fallbackRoute", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                        .body("Service Unavailable, please try again later"))
                 .build();
     }
 
--- order-service/src/main/java/com/arc/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/arc/microservices/order/client/InventoryClient.java
@@ -1,13 +1,33 @@
 package com.arc.microservices.order.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import groovy.util.logging.Slf4j;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 
-@FeignClient(value = "inventory", url = "${inventory.url}")
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
+
+import java.lang.annotation.Retention;
+
+//@FeignClient(value = "inventory", url = "${inventory.url}") // without CB
+@Slf4j
 public interface InventoryClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
+
+    @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
+    //@RequestMapping(method = RequestMethod.GET, value = "/api/inventory") // without CB
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String code, Integer quantity, Throwable throwable) {
+        log.info("Cannot get inventory for skuCode: {} and quantity: {}, failure reason: {}", code, quantity, throwable.getMessage());
+        return false;
+    }
 }
