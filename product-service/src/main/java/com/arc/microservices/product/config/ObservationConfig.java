01a3d3db558e02166dfe578c7ea69ae53b09b229
ARC-22 <advaitchuri22@gmail.com>
2025-03-25 09:37:46 -0700

    update api gateway and product service observability.

--- product-service/src/main/java/com/arc/microservices/product/config/ObservationConfig.java
+++ product-service/src/main/java/com/arc/microservices/product/config/ObservationConfig.java
@@ -1,19 +1,5 @@
 package com.arc.microservices.product.config;
 
-//import io.micrometer.observation.ObservationRegistry;
-//import io.micrometer.observation.aop.ObservedAspect;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class ObservationConfig {
-//    @Bean
-//    ObservedAspect observedAspect(ObservationRegistry registry) {
-//        return new ObservedAspect(registry);
-//    }
-//}
-
-
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.aop.ObservedAspect;
 import org.springframework.context.annotation.Bean;
